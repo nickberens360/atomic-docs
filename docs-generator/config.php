9e0f2c7d9593e0692caee4a77e1e44738a7a021c
Nick Berens <nberens@wisnet.com>
2014-08-14 15:53:24 -0500

    removed rename branch

---
+++ docs-generator/config.php
@@ -0,0 +1,42 @@
+<?php
+
+
+
+//PARTIAL DIRECTORY NAME E.G: MODULES, PARTIALS, LAYOUTS, ETC..
+$partialDirOne = "atoms";	
+$partialDirTwo = "molecules";	
+$partialDirThree = "organisms";
+$partialDirFour = "utilities";
+$partialDirFive = "templates";
+
+
+//ROOT SASS PARTIAL DIRECTORY NAME E.G: SASS, SCSS, ETC..
+$root = "scss";
+//PARTIAL PATH (NO NEED TO EDIT)
+$partDirOnePath = "../$root/$partialDirOne";
+$partDirTwoPath = "../$root/$partialDirTwo";
+$partDirThreePath = "../$root/$partialDirThree";
+$partDirFourPath = "../$root/$partialDirFour";
+$partDirFivePath = "../$root/$partialDirFive";
+$rootPath = "../$root";
+
+
+
+
+//ROOT PHP COMPONENT DIRECTORY NAME E.G: COMPONENTS
+$compRoot = "components";
+
+//PARTIAL PATH (NO NEED TO EDIT)
+$compRootPath = "../$compRoot";
+
+$compDirOnePath = "../$compRoot/$partialDirOne";
+$compDirTwoPath = "../$compRoot/$partialDirTwo";
+$compDirThreePath = "../$compRoot/$partialDirThree";
+$compDirFourPath = "../$compRoot/$partialDirFour";
+$compDirFivePath = "../$compRoot/$partialDirFive";
+
+
+
+
+
+?>
