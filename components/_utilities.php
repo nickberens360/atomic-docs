7560dd8faf3133e66e687cfb3438abdc4e248178
Nick Berens <nberens@wisnet.com>
2014-08-23 18:05:07 -0500

    more/less added

--- components/_utilities.php
+++ components/_utilities.php
@@ -1,2 +1,1 @@
 <span id="block-grid"></span><div class="component"><?php include ("components/utilities/block-grid.php");?></div>
-<span id="test"></span><div class="component"><?php include ("components/utilities/test.php"); ?></div>
