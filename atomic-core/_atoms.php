f211b804be5fb13c39948b344fb47d0e6d9dd57c
Nick Berens <nberens@wisnet.com>
2014-08-31 11:42:48 -0500

    replaced master files with classes branch files

--- atomic-core/_atoms.php
+++
@@ -1,6 +0,0 @@
-<<<<<<< HEAD
-<span id="test"></span><div class="component"><?php include ("../components/atoms/test.php"); ?></div>
-<span id="new-component"></span><div class="component"><?php include ("../components/atoms/new-component.php"); ?></div>
-=======
-<span id="test"></span><div class="component"><?php include("../components/atoms/test.php");?></div>
->>>>>>> classes
--- components/atoms/new-component.php
+++
@@ -1,1 +0,0 @@
-<div class="new">New</div>
