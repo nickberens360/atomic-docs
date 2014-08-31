1ba1be523db582b787fd3e087f32268d936e9cc9
Nick Berens <nberens@wisnet.com>
2014-08-30 22:40:01 -0500

    deleted test components

--- atomic-core/_atoms.php
+++
@@ -1,4 +0,0 @@
-<span id="test"></span><div class="component"><?php include("../components/atoms/test.php");?></div>
-<span id="new"></span><div class="component"><?php include("../components/atoms/new.php");?></div>
-<span id="more"></span><div class="component"><?php include("../components/atoms/more.php");?></div>
-<span id="testttt"></span><div class="component"><?php include("../components/atoms/testttt.php");?></div>
