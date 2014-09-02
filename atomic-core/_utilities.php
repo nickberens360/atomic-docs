6def9a1e538a208b7ed9182ddf4fadb787042b0e
Nick Berens <nberens@wisnet.com>
2014-09-02 16:41:57 -0500

    added show-hide component

--- atomic-core/_utilities.php
+++ atomic-core/_utilities.php
@@ -1,1 +1,2 @@
 <span id="block-grid"></span><div class="component"><?php include ("../components/utilities/block-grid.php");?></div>
+<span id="show-hide"></span><div class="component"><?php include("../components/utilities/show-hide.php");?></div>
---
+++ components/utilities/show-hide.php
@@ -0,0 +1,43 @@
+<section class="atoms-group">
+	<div class="source">
+
+<div class="show-hide">
+	<div class="show-hide_content">
+		hidden content
+	</div>
+	<span class="show-hide_btn">More</span>
+	<span class="show-hide_cls">Less</span>
+</div>
+
+<div class="show-hide">
+	<div class="show-hide_content">
+		hidden content
+	</div>
+	<span class="show-hide_btn">More</span>
+	<span class="show-hide_cls">Less</span>
+</div>
+
+<div class="show-hide">
+	<div class="show-hide_content">
+		hidden content
+	</div>
+	<span class="show-hide_btn">More</span>
+	<span class="show-hide_cls">Less</span>
+</div>
+
+<div class="show-hide">
+	<div class="show-hide_content">
+		hidden content
+	</div>
+	<span class="show-hide_btn">More</span>
+	<span class="show-hide_cls">Less</span>
+</div>
+
+	</div>
+	<div class="atoms-code-example">
+		<pre><code class="dest language-markup">
+			
+		</code></pre>
+	</div>
+</section>
+	
