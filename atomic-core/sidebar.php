950dfc3d61edf89e39f2d6ecd52cc57bd28daf48
Nick Berens <nberens@wisnet.com>
2014-09-22 07:29:21 -0500

    css fix

--- atomic-core/sidebar.php
+++ atomic-core/sidebar.php
@@ -15,16 +15,22 @@
 		<ul class="atoms-nav">
 			<li class="atom-side_head"><a href="#">Components</a></li>
 			<?php include ("includes/_sidebar.php");?>
-			<li>
-				<form class="atomic-sub-form " action="atomic-core/partial-mngr/create-category.php" method="post">
-					<input type="text" class="form-control" id="inputDefault" name="inputName" placeholder="Create Category">
-				</form>
+			<li class="show-hide">
+				<span class="cat-form-btn show-hide_btn">+ Create New Category</span>
+				<span class="cat-form-btn show-hide_cls">- Create New Category</span>
+				<div class="cat-form show-hide_content">
+					<form class="atomic-sub-form " action="atomic-core/partial-mngr/create-category.php" method="post">
+						<input type="text" class="form-control" id="inputDefault" name="inputName" placeholder="Create Category">
+					</form>
+			
+					<form class="atomic-sub-form " action="atomic-core/partial-mngr/delete-category.php" method="post">
+						<input type="text" class="form-control" id="inputDefault" name="inputName" placeholder="Delete Category">
+					</form>
+				</div>
 			</li>
-			<li>
-				<form class="atomic-sub-form " action="atomic-core/partial-mngr/delete-category.php" method="post">
-					<input type="text" class="form-control" id="inputDefault" name="inputName" placeholder="Delete Category">
-				</form>
-			</li>
+			
+			
 		</ul>
 	</nav>
+	
 </aside>
--- components/utilities/show-hide.php
+++ components/utilities/show-hide.php
@@ -1,5 +1,4 @@
-<section class="atoms-group">
-	<div class="source">
+
 
 <div class="show-hide">
 	<div class="show-hide_content">
@@ -33,11 +32,5 @@
 	<span class="show-hide_cls">Less</span>
 </div>
 
-	</div>
-	<div class="atoms-code-example">
-		<pre><code class="dest language-markup">
-			
-		</code></pre>
-	</div>
-</section>
+
 	
