f7b38133fe37127ea149bcfe57fccb948a84d870
nickberens360 <nickberens360@gmail.com>
2014-11-02 09:56:17 -0600

    fixed show-hide component

--- components/utilities/show-hide.php
+++ components/utilities/show-hide.php
@@ -1,6 +1,3 @@
-<section class="atoms-group">
-	<div class="source">
-
 <div class="show-hide">
 	<div class="show-hide_content">
 		hidden content
@@ -8,36 +5,4 @@
 	<span class="show-hide_btn">More</span>
 	<span class="show-hide_cls">Less</span>
 </div>
-
-<div class="show-hide">
-	<div class="show-hide_content">
-		hidden content
-	</div>
-	<span class="show-hide_btn">More</span>
-	<span class="show-hide_cls">Less</span>
-</div>
-
-<div class="show-hide">
-	<div class="show-hide_content">
-		hidden content
-	</div>
-	<span class="show-hide_btn">More</span>
-	<span class="show-hide_cls">Less</span>
-</div>
-
-<div class="show-hide">
-	<div class="show-hide_content">
-		hidden content
-	</div>
-	<span class="show-hide_btn">More</span>
-	<span class="show-hide_cls">Less</span>
-</div>
-
-	</div>
-	<div class="atoms-code-example">
-		<pre><code class="dest language-markup">
-			
-		</code></pre>
-	</div>
-</section>
 	
