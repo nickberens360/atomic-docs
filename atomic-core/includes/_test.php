3a34a4b7b0693da2f0fa2959d39ad99af610f406
nickberens360 <nickberens360@gmail.com>
2014-12-07 12:57:59 -0600

    test cat

--- atomic-core/includes/_test.php
+++
@@ -1,2 +0,0 @@
-<span id="new"></span><div class="component"><?php include("../components/test/new.php");?></div>
-<span id="more"></span><div class="component"><?php include("../components/test/more.php");?></div>
