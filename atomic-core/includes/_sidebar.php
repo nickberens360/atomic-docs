30a5266bfebbc655329dc9ef0bd803f4a2843f7a
nickberens360 <nickberens360@gmail.com>
2014-12-07 12:53:49 -0600

    test cat creation

--- atomic-core/includes/_sidebar.php
+++ atomic-core/includes/_sidebar.php
@@ -151,4 +151,41 @@
 		</ul>
 </li>
 				
-
+<li>
+		<div class="atomic-link-grp">
+			<span class="atomic-link-show atomic-link-show-test">+</span>
+			<a class="atomic-link-main" href="atomic-core/test.php">test</a>
+		</div>
+		<ul class="atoms-sub-menu atoms-sub-test">
+		<li class="atomic-subform-group">
+				<form class="atomic-sub-form " action="atomic-core/partial-mngr/create.php" method="post">
+					<input type="text" class="form-control" id="inputDefault" name="compName" placeholder="Create Component">
+					<input type="hidden" name="compDir" value="test"/>
+				</form>
+				<form class="atomic-sub-form " action="atomic-core/partial-mngr/delete.php" method="post">
+					<input type="text" class="form-control" id="inputDefault" name="compName" placeholder="Delete Component">
+					<input type="hidden" name="compDir" value="test"/>
+				</form>
+		</li>
+		<?php
+			$orig = "../components/test";
+			if ($dir = opendir($orig)) {
+			while ($file = readdir($dir)) {
+			$ok = "true";	
+			$filename = $file;
+			$filename = basename($filename, ".php");
+			if ($file == "."){
+			$ok = "false";
+			}
+			else if ($file == ".."){
+			$ok = "false";	
+			}
+			if ($ok == "true"){
+			echo "<li><a href='#$filename'>$filename</a></li>";
+			}
+			}
+			closedir($dir);
+			}
+		?>
+		</ul>
+</li>
---
+++ atomic-core/includes/_test.php
@@ -0,0 +1,2 @@
+<span id="new"></span><div class="component"><?php include("../components/test/new.php");?></div>
+<span id="more"></span><div class="component"><?php include("../components/test/more.php");?></div>
