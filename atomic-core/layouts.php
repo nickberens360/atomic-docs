cdfc765f1ec1c887552cc4ee2fe040ee691570c4
nickberens360 <nickberens360@gmail.com>
2015-09-26 06:21:57 -0500

    demo

---
+++ atomic-core/layouts.php
@@ -0,0 +1,19 @@
+<?php include ("head.php");?>
+	<body class="layouts">
+	
+	
+	<div class="grid-row atoms-container">
+			<?php include ("sidebar.php");?>
+			
+			
+			<div class="atoms-main">
+					<h1 id="layouts" class="atomic-h1">layouts</h1>
+	
+	
+							<?php include ("includes/_layouts.php");?>
+	
+	
+			</div>
+	</div>
+	
+	<?php include ("footer.php");?>
---
+++ atomic-core/modules.php
@@ -0,0 +1,19 @@
+<?php include ("head.php");?>
+	<body class="modules">
+	
+	
+	<div class="grid-row atoms-container">
+			<?php include ("sidebar.php");?>
+			
+			
+			<div class="atoms-main">
+					<h1 id="modules" class="atomic-h1">modules</h1>
+	
+	
+							<?php include ("includes/_modules.php");?>
+	
+	
+			</div>
+	</div>
+	
+	<?php include ("footer.php");?>
