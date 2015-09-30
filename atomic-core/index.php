700aa430b242bc2a6701f42dfa0eb5f2d6476d22
nickberens360 <nickberens360@gmail.com>
2015-09-29 20:49:28 -0500

    worked on documentation

--- atomic-core/index.php
+++ atomic-core/index.php
@@ -9,8 +9,10 @@
 	<div class="atoms-main">
 		<div class="introBlock">
 			<h1 class="introBlock-heading">Welcome!</h1>
-			<p>Place the atomic-docs folder in the directory where you serve your files from in your local php (yes, php only for the moment) development environment. You can rename "atomic-docs" to the name of your project, it makes no difference.<p> 
+			<p>Place the atomic-docs folder in the directory of your local dev enviornment.<p> 
 			<p>To get started create a new category in the lower left corner. For example, Base, Organisms, Layout or whatever.</p>
+			<p>Click on the category link you just created. From there created a new component.</p>
+			<p>Now have a look at your folder structure. You will notice a folder named "components". Inside components will be a folder with the name of the category you created, and within that will be all the html component files you create that fall under that category.</p>
 		</div>
 
 	</div>
