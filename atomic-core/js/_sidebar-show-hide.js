0a21a7a9cba42656d85f8e83948479cd2a5ac278
nickberens360 <nickberens360@gmail.com>
2015-10-04 10:49:20 -0500

    fix cat-form hide-show

--- atomic-core/js/_sidebar-show-hide.js
+++ atomic-core/js/_sidebar-show-hide.js
@@ -2,8 +2,10 @@ function sidebar(){
 	var $side = $('.atoms-side');
 	var $main = $('.atoms-main');
 	var $showBtn = $('.atoms-side_show');
+	var $catForm = $('.cat-form-group');
 	
 	$showBtn.css('left', '-100px');
+	$catForm.css('left', '0');
 	$side.css('left', '0');
 	sideBarWidth = $( ".atoms-side_hide" ).outerWidth();
 	
@@ -14,8 +16,10 @@ function noSidebar(){
 	var $side = $('.atoms-side');
 	var $main = $('.atoms-main');
 	var $showBtn = $('.atoms-side_show');
+	var $catForm = $('.cat-form-group');
 	
 	$showBtn.css('left', '10px');
+	$catForm.css('left', '-500px');
 	$side.css('left', '-500px');
 	$main.css('padding-left', '20px').css('width', '100%');
 }
