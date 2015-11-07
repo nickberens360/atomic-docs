de752e872a52f68602555b456f2e033ce3ee31c7
nickberens360 <nickberens360@gmail.com>
2015-11-07 17:43:16 -0600

    testing

---
+++ atomic-core/test.php
@@ -0,0 +1,19 @@
+<?php include ("head.php");?>
+	<body class="test">
+	
+	
+	<div class="grid-row atoms-container">
+			<?php include ("sidebar.php");?>
+			
+			
+			<div class="atoms-main">
+					<h1 id="test" class="atomic-h1">test</h1>
+	
+	
+							<?php include ("includes/_test.php");?>
+	
+	
+			</div>
+	</div>
+	
+	<?php include ("footer.php");?>
