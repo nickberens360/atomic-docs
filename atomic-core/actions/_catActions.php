01d762d82cb1cd6dbdf22b89c79cf421816f8f30
nickberens360 <nickberens360@gmail.com>
2015-11-12 07:07:04 -0600

    form styling

--- atomic-core/actions/_catActions.php
+++ atomic-core/actions/_catActions.php
@@ -1,11 +1,20 @@
 <h1>Cat Actions</h1>
 
 <form class="ad_fileForm " action="/atomic-docs/atomic-core/index.php" method="post">
-  <input type="text" class="form-control" name="inputName" placeholder="Create Category" required>
+  <div class="formInputGroup">
+	  <label class="ad_label">Add a category</label>
+	  <div class="inputBtnGroup">
+	    <div class="fcWrap"><input type="text" class="form-control" name="inputName" required></div>
+        <button class="ad_btn" type="submit">Submit</button>
+	  </div>
+  </div>
   <input type="hidden" name="createDir" value="createDir"/>
 </form>
 
 <form class="ad_fileForm " action="/atomic-docs/atomic-core/index.php" method="post" onsubmit="return confirm('Deleting a category deletes all files within it as well. Are you sure about this?');">
-  <input type="text" class="form-control" name="inputName" placeholder="Delete Category" required>
+  <div class="formInputGroup">
+	  <label class="ad_label">Delete a category</label>
+	  <input type="text" class="form-control" name="inputName" placeholder="Must type category name" required>
+  </div>
   <input type="hidden" name="deleteDir" value="deleteDir"/>
 </form>
