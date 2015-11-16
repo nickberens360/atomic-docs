19419e6305ec38ab3e915eed3b20a7053618697d
nickberens360 <nickberens360@gmail.com>
2015-11-16 06:39:26 -0600

    testing

---
+++ atomic-core/actions/layouts/_ajax-layouts.php
@@ -0,0 +1,39 @@
+<!-- <div class="ad_fileFormGroup">
+    <form class="ad_fileForm " action="/atomic-docs/atomic-core/layouts.php" method="post">
+      <div class="inputBtnGroup">
+        <label class="ad_label">What's your component's name?</label>
+        <button class="ad_btn ad_btn-pos" type="submit" >Add</button>
+        <div class="inputBtnGroup__inputWrap"><input type="text" class="form-control" name="compName" required></div>
+      </div>
+      <input type="hidden" name="compDir" value="layouts"/>
+      <input type="hidden" name="create" value="create"/>
+    </form>
+</div> -->
+
+
+<form action="/atomic-docs/atomic-core/layouts/_ajax-layouts.php" method="POST">
+        
+        <!-- NAME -->
+        <div id="name-group" class="form-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" name="name" placeholder="Henry Pym">
+            <!-- errors will go here -->
+        </div>
+
+        <!-- EMAIL -->
+        <div id="email-group" class="form-group">
+            <label for="email">Email</label>
+            <input type="text" class="form-control" name="email" placeholder="rudd@avengers.com">
+            <!-- errors will go here -->
+        </div>
+
+        <!-- SUPERHERO ALIAS -->
+        <div id="superhero-group" class="form-group">
+            <label for="superheroAlias">Superhero Alias</label>
+            <input type="text" class="form-control" name="superheroAlias" placeholder="Ant Man">
+            <!-- errors will go here -->
+        </div>
+
+        <button type="submit" class="btn btn-success">Submit <span class="fa fa-arrow-right"></span></button>
+
+    </form>
--- atomic-core/partial-mngr/create.php
+++ atomic-core/partial-mngr/create.php
@@ -2,9 +2,8 @@
 
 
 
-if (!empty($_POST['create'])){
+/*if (!empty($_POST['create'])){
 
-//Set user input variable
 
 $fileName = test_input($_POST["compName"]);
 $catName = test_input($_POST["compDir"]);
@@ -22,8 +21,51 @@ createIncludeString($catName, $fileName );
 createAjaxIncludeAndCompFile($catName, $fileName);
 
 header("location: http://127.0.0.1/atomic-docs/atomic-core/$catName.php");
-}
+}*/
 
+
+?>
+
+<?php
+// process.php
+
+$errors         = array();      // array to hold validation errors
+$data           = array();      // array to pass back data
+
+// validate the variables ======================================================
+    // if any of these variables don't exist, add an error to our $errors array
+
+    if (empty($_POST['name']))
+        $errors['name'] = 'Name is required.';
+
+    if (empty($_POST['email']))
+        $errors['email'] = 'Email is required.';
+
+    if (empty($_POST['superheroAlias']))
+        $errors['superheroAlias'] = 'Superhero alias is required.';
+
+// return a response ===========================================================
+
+    // if there are any errors in our errors array, return a success boolean of false
+    if ( ! empty($errors)) {
+
+        // if there are items in our errors array, return those errors
+        $data['success'] = false;
+        $data['errors']  = $errors;
+    } else {
+
+        // if there are no errors process our form, then return a message
+
+        // DO ALL YOUR FORM PROCESSING HERE
+        // THIS CAN BE WHATEVER YOU WANT TO DO (LOGIN, SAVE, UPDATE, WHATEVER)
+
+        // show a message of success and provide a true success variable
+        $data['success'] = true;
+        $data['message'] = 'Success!';
+    }
+
+    // return all our data to an AJAX call
+    echo json_encode($data);
 
 ?>
 
