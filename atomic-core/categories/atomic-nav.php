50aa39948593f06c4a471168d07c5838a30a2459
nickberens360 <nickberens360@gmail.com>
2015-12-01 21:51:31 -0600

    removed empty directories

--- atomic-core/categories/atomic-nav.php
+++ atomic-core/categories/atomic-nav.php
@@ -1,7 +1,1 @@
 <?php include ('base/navItem-base.php');?>
-<?php include ('layout/navItem-layout.php');?>
-<?php include ('modules/navItem-modules.php');?>
-<?php include ('objects/navItem-objects.php');?>
-<?php include ('utilities/navItem-utilities.php');?>
-<?php include ('variables/navItem-variables.php');?>
-<?php include ('mixins/navItem-mixins.php');?>
