c0d87417ccd48c79e19c71c0a222663d601df316
nickberens360 <nickberens360@gmail.com>
2015-12-26 10:04:32 -0600

    test

--- atomic-core/categories/modules/modules.php
+++
@@ -1,1 +0,0 @@
-<span id="box" class="compTitle">box</span><p class="compNotes">This is the description for the box element.</p><div class="component"><?php include("../components/modules/box.php");?></div>
