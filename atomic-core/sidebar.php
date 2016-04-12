14abb8476eb355e2718cd57b1d8c5f0a182d163c
Nick Berens <nberens@wisnet.com>
2016-04-12 14:06:11 -0500

    change icon arrangement

--- atomic-core/sidebar.php
+++ atomic-core/sidebar.php
@@ -23,10 +23,9 @@
 	<div class="atoms-side_hide">
 		<span class="js-hideSide fa fa-arrow-left"></span>
 		<span class="js-hideTitle fa fa-header"></span>
-        </span><span class="js-hideNotes fa fa-paragraph"></span>
+    </span><span class="js-hideNotes fa fa-paragraph"></span>
 		</span><span class="js-hideCode fa fa-code"></span>
     </div>
-	<!-- <span class="atoms-side_hide">Hide Sidebar</span> -->
 
 	<nav>
 		<ul class="atoms-nav">
