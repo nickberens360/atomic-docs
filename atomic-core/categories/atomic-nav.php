5d329d82a33d6a5cb6f044bd73c8e9e22fdd2c79
Nick Berens <nberens@wisnet.com>
2016-04-13 07:44:33 -0500

    hide notes toggle fix

--- atomic-core/categories/atomic-nav.php
+++ atomic-core/categories/atomic-nav.php
@@ -1,2 +1,1 @@
 <?php include ('modules/navItem-modules.php');?>
-<?php include ('objects/navItem-objects.php');?>
--- atomic-core/js/hideNotes.js
+++ atomic-core/js/hideNotes.js
@@ -4,7 +4,7 @@
 
 
 
-if ($(".compNotes").is(':empty')){
+/*if ($(".compNotes").is(':empty')){
 
 }
 else {
@@ -27,4 +27,24 @@ else {
     }
   });
 
+}*/
+
+if ($(".atoms-code-example")[0]){
+    $(".js-hideNotes").on('click', function(event) {
+      if($('.compNotes').css('opacity') == '0'){ 
+         $(this).css('color','#fff');
+         $(".compNotes").velocity({
+            opacity: "1",
+         }, {
+            duration: 200
+         });
+      } else { 
+         $(this).css('color','#247695');
+         $(".compNotes").velocity({
+            opacity: "0",
+         }, {
+            duration: 200
+         });
+      }
+    });
 }
