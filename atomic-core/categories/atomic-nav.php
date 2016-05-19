7d866a74a6f8f9b4513f182cb228b664adc34b76
nickberens360 <nickberens360@gmail.com>
2016-05-18 22:00:46 -0500

    remove example components

--- atomic-core/categories/atomic-nav.php
+++ atomic-core/categories/atomic-nav.php
@@ -1,4 +1,1 @@
-<?php include ('layout/navItem-layout.php');?>
-<?php include ('atoms/navItem-atoms.php');?>
-<?php include ('molecules/navItem-molecules.php');?>
-<?php include ('organisms/navItem-organisms.php');?>
+<?php include ('modules/navItem-modules.php');?>
