4d7c4c2c4efc04a585d251a46e8b45d5ba8ca7e0
Nick Berens <nberens@wisnet.com>
2016-06-15 16:22:30 -0500

    add logos

--- js/main.js
+++ js/main.js
@@ -4,12 +4,16 @@ jQuery( document ).ready(function( $ ) {
 $(".vidWrap").fitVids();
 
 /* $('.menu').dropit();
- 
- 
+
+
  $( '.swipebox' ).swipebox();*/
- 
+
  $('.mh-item').matchHeight();
 
+
+
+  $(".section:even").css("background-color", "#D7F3F6");
+  $(".section:odd").css("background-color", "#ffffff");
 
 
 
@@ -126,20 +130,20 @@ particlesJS("particles-js", {
   },
   "retina_detect": true
 });
-var count_particles, stats, update;
-stats = new Stats;
-stats.setMode(0);
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector('.js-count-particles');
-update = function() {
-  stats.begin();
-  stats.end();
-  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-  }
-  requestAnimationFrame(update);
-};
-requestAnimationFrame(update);;
+// var count_particles, stats, update;
+// stats = new Stats;
+// stats.setMode(0);
+// stats.domElement.style.position = 'absolute';
+// stats.domElement.style.left = '0px';
+// stats.domElement.style.top = '0px';
+// document.body.appendChild(stats.domElement);
+// count_particles = document.querySelector('.js-count-particles');
+// update = function() {
+//   stats.begin();
+//   stats.end();
+//   if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+//     count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+//   }
+//   requestAnimationFrame(update);
+// };
+// requestAnimationFrame(update);
